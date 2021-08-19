3210f6d48e8ceaac41626a0afb96b9100599d189
Harshit Verma <harshitv9984@gmail.com>
2021-08-19 18:57:10 +0530

    Minimum no. of Jumps to reach end of an array

--- Trie/Test.cpp
+++ Trie/Test.cpp
@@ -1,17 +1,100 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
-int main() {
-	int t;
-	cin >> t;
+#define MAX 10
 
-	while (t--) {
-		string num;
-		cin >> num;
+void enqueue(int *, int, int *, int *);
+int dequeue(int *, int *, int *);
+void traverse(int *);
 
-		num[0] != '1' ? cout << "1" + num : cout << num[0] << "0" << num.substr(1);
-		cout << endl;
+int main()
+{
+	int arr[MAX];
+	int front, rear, i, n;
+
+	front = rear = -1;
+	for (i = 0; i < MAX; i++)
+		arr[i] = 0;
+
+	enqueue(arr, 5, &front, &rear);
+	enqueue(arr, 11, &front, &rear);
+	enqueue(arr, 6, &front, &rear);
+
+	printf("\nElements in a deque: ");
+	traverse(arr);
+}
+
+void enqueue(int *arr, int item, int *pfront, int *prear)
+{
+	int i, k;
+
+	if (*pfront == 0 && *prear == MAX - 1)
+	{
+		printf("\nDeque is full.\n");
+		return;
 	}
 
-	return 0;
+	if (*pfront == -1)
+	{
+		*prear = *pfront = 0;
+		arr[*prear] = item;
+		return;
+	}
+
+	if (*prear == MAX - 1)
+	{
+		k = *pfront - 1;
+		for (i = *pfront - 1; i < *prear; i++)
+		{
+			k = i;
+			if (k == MAX - 1)
+				arr[k] = 0;
+			else
+				arr[k] = arr[i + 1];
+		}
+		(*prear)--;
+		(*pfront)--;
+	}
+	(*prear)++;
+	arr[*prear] = item;
+}
+
+int dequeue(int *arr, int *pfront, int *prear)
+{
+	int item;
+
+	if (*pfront == -1)
+	{
+		printf("\nDeque is empty.\n");
+		return 0;
+	}
+
+	item = arr[*pfront];
+	arr[*pfront] = 0;
+
+	if (*pfront == *prear)
+		*pfront = *prear = -1;
+	else
+		(*pfront)++;
+
+	return item;
+}
+
+void traverse(int *arr)
+{
+	printf("\n front:  ");
+	for (int i = 0; i < MAX; i++)
+		printf("  %d", arr[i]);
+	printf("  :rear");
+}
+
+int count(int *arr)
+{
+	int c = 0, i;
+
+	for (i = 0; i < MAX; i++)
+	{
+		if (arr[i] != 0)
+			c++;
+	}
+	return c;
 }
