11d5d4ea906a4d5d37beedc059f3aa6e96dfca84
Harshit Verma <harshitvermaofficial@gmail.com>
2025-05-27 00:13:43 +0530

    ds(heap): improved MaxHeap

--- Practice/Heap/MaxHeap.js
+++ Practice/Heap/MaxHeap.js
@@ -3,109 +3,127 @@ class MaxHeap {
     this.arr = [];
   }
 
-  isEmpty = () => {
+  isEmpty() {
     return this.arr.length === 0;
-  };
-
-  getLeftChildIndex = (index) => {
-    return index * 2 + 1;
-  };
-
-  getRightChildIndex = (index) => {
-    return index * 2 + 2;
-  };
-
-  hasLeftChild = (index) => {
-    return this.getLeftChildIndex(index) < this.arr.length;
-  };
-
-  hasRightChild = (index) => {
-    return this.getRightChildIndex(index) < this.arr.length;
-  };
-
-  getParentIndex = (index) => {
-    return Math.floor((index - 1) / 2);
-  };
-
-  swap(indexA, indexB) {
-    const temp = this.arr[indexA];
-    this.arr[indexA] = this.arr[indexB];
-    this.arr[indexB] = temp;
   }
 
-  bubbleDown = (index) => {
-    const heapSize = this.arr.length;
-
-    while (index < heapSize) {
-      const leftChildIndex = this.getLeftChildIndex(index);
-      const rightChildIndex = this.getRightChildIndex(index);
-      let biggerChildIndex = index;
-
-      if (
-        leftChildIndex < heapSize &&
-        this.arr[leftChildIndex] > this.arr[biggerChildIndex]
-      ) {
-        biggerChildIndex = leftChildIndex;
-      }
-
-      if (
-        rightChildIndex < heapSize &&
-        this.arr[rightChildIndex] > this.arr[biggerChildIndex]
-      ) {
-        biggerChildIndex = rightChildIndex;
-      }
-
-      if (biggerChildIndex === index) {
-        break;
-      }
-
-      this.swap(index, biggerChildIndex);
-      index = biggerChildIndex;
-    }
-  };
-
-  bubbleUp = (index) => {
-    while (index > 0) {
-      const parentIndex = this.getParentIndex(index);
-
-      if (this.arr[parentIndex] < this.arr[index]) {
-        this.swap(parentIndex, index);
-        index = parentIndex;
-      } else {
-        break;
-      }
-    }
-  };
-
-  insert = (value) => {
-    this.arr.push(value);
-    this.bubbleUp(this.arr.length - 1);
-  };
-
-  remove = () => {
-    if (this.isEmpty()) {
-      return null;
-    }
-
-    const removedItem = this.arr[0];
-    const lastItem = this.arr.pop();
-
-    if (this.isEmpty()) {
-      return removedItem;
-    }
-
-    this.arr[0] = lastItem;
-    this.bubbleDown(0);
-
-    return removedItem;
-  };
-
-  peek = () => {
+  peek() {
     return this.arr[0];
-  };
+  }
+
+  getLeftChildIndex(index) {
+    return index * 2 + 1;
+  }
+
+  getRightChildIndex(index) {
+    return index * 2 + 2;
+  }
+
+  hasLeftChild(index) {
+    return this.getLeftChildIndex(index) < this.arr.length;
+  }
+
+  hasRightChild(index) {
+    return this.getRightChildIndex(index) < this.arr.length;
+  }
+
+  getLeftChild(index) {
+    return this.arr[this.getLeftChildIndex(index)];
+  }
+
+  getRightChild(index) {
+    return this.arr[this.getRightChildIndex(index)];
+  }
+
+  getParentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  isGreaterThanParent(index) {
+    return this.arr[index] > this.arr[this.getParentIndex(index)];
+  }
+
+  isValidParent(index) {
+    if (!this.hasLeftChild(index)) {
+      return true;
+    }
+
+    const currentValue = this.arr[index];
+    let isValid = currentValue >= this.getLeftChild(index);
+
+    if (this.hasRightChild(index)) {
+      isValid &&= currentValue >= this.getRightChild(index);
+    }
+
+    return isValid;
+  }
+
+  getBiggerChildIndex(index) {
+    if (!this.hasLeftChild(index)) {
+      return index;
+    }
+
+    if (!this.hasRightChild(index)) {
+      return this.getLeftChildIndex(index);
+    }
+
+    return this.getLeftChild(index) > this.getRightChild(index)
+      ? this.getLeftChildIndex(index)
+      : this.getRightChildIndex(index);
+  }
+
+  swap(sourceIndex, targetIndex) {
+    const temp = this.arr[sourceIndex];
+    this.arr[sourceIndex] = this.arr[targetIndex];
+    this.arr[targetIndex] = temp;
+  }
+
+  bubbleUp() {
+    let currentIndex = this.arr.length - 1;
+
+    while (currentIndex > 0 && this.isGreaterThanParent(currentIndex)) {
+      const parentIndex = this.getParentIndex(currentIndex);
+      this.swap(currentIndex, parentIndex);
+      currentIndex = parentIndex;
+    }
+  }
+
+  bubbleDown() {
+    let currentIndex = 0;
+
+    while (
+      currentIndex < this.arr.length &&
+      !this.isValidParent(currentIndex)
+    ) {
+      const biggerChildIndex = this.getBiggerChildIndex(currentIndex);
+      this.swap(currentIndex, biggerChildIndex);
+      currentIndex = biggerChildIndex;
+    }
+  }
+
+  insert(value) {
+    this.arr.push(value);
+    this.bubbleUp();
+  }
+
+  remove() {
+    if (this.isEmpty()) return undefined;
+
+    const removedValue = this.arr[0];
+    const lastValue = this.arr.pop();
+
+    if (this.isEmpty()) {
+      return removedValue;
+    }
+
+    this.arr[0] = lastValue;
+    this.bubbleDown();
+
+    return removedValue;
+  }
 }
 
-const maxHeap = new MaxHeap(9);
+const maxHeap = new MaxHeap();
 
 [15, 10, 3, 8, 12, 9, 4, 1, 24].forEach((value) => maxHeap.insert(value));
 
